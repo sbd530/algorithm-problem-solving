7fe84167447568c9be9d7a62bba9f00c4c7eb0b9
don <byungdon92@gmail.com>
2021-10-12 23:36:06 +0900

    2021.10.12

---
+++ src/main/java/baekjoon/dp1/boj2225/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.dp1.boj2225;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long mod = 1_000_000_000;
+        long[][] d = new long[k + 1][n + 1];
+
+        d[0][0] = 1;
+        for (int i = 1; i <= k; i++)
+            for (int j = 0; j <= n; j++)
+                for (int l = 0; l <= j; l++) {
+                    d[i][j] += d[i - 1][j - l];
+                    d[i][j] %= mod;
+                }
+        System.out.println(d[k][n]);
+    }
+}
