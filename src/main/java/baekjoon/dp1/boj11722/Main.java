18256cf714222e7945b8fbaba9988e873c53990c
don <byungdon92@gmail.com>
2021-10-16 22:02:32 +0900

    2021.10.16

--- src/main/java/baekjoon/dp1/boj11722/Main.java
+++ src/main/java/baekjoon/dp1/boj11722/Main.java
@@ -1,4 +1,4 @@
-package baekjoon.dp1.boj11053;
+package baekjoon.dp1.boj11722;
 
 import java.util.Scanner;
 
@@ -14,7 +14,7 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             d[i] = 1;
             for (int j = 1; j <= i; j++) {
-                if (a[j] < a[i] && d[i] < d[j] + 1)
+                if (a[j] > a[i] && d[i] < d[j] + 1)
                     d[i] = d[j] + 1;
             }
         }
