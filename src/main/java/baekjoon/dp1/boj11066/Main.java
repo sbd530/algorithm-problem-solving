472d2c4fec76a29eb1ca46e7173004d76d7f2d82
don <byungdon92@gmail.com>
2021-10-26 00:34:19 +0900

    2021.10.26

--- src/main/java/baekjoon/dp1/boj11066/Main.java
+++ src/main/java/baekjoon/dp1/boj11066/Main.java
@@ -8,10 +8,9 @@ public class Main {
      * dp[i][j] == i부터 j번째 파일을 통합할 때 필요한 최소 비용  [i <= j]
      * dp[i][i] == a[i]
      * dp[i][i+1] == a[i]+a[i+1]
-     * dp[i][i+2] == min{ a[i]+a[i+1], a[i+1]+a[i+2] } + { a[i]+a[i+1]+a[i+2] }
-     * dp[i][j] == min{ dp[i][k]+dp[i][k+1] } + { a[i]+...+a[j] }   [k-LOOP : i ~ j-1]
+     * dp[i][i+2] == min{ a[i]+a[i+1], a[i+1]+a[i+2] } + sum{ a[i], a[i+1], a[i+2] }
+     * dp[from][to] == min{ dp[from][i]+dp[i+1][to] } + sum{ a[from], ..., a[to] }   [i-LOOP : from <= i < to]
      */
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
@@ -19,26 +18,25 @@ public class Main {
         while (t-- > 0) {
             int n = sc.nextInt();
             int[] a = new int[n + 1];
-            int[][] cache = new int[n + 1][n + 1];
+            int[] sum = new int[n + 1]; // sum 캐시
+            int[][] cache = new int[n + 1][n + 1]; // dp 캐시
             for (int i = 1; i <= n; i++) {
                 a[i] = sc.nextInt();
+                sum[i] = sum[i - 1] + a[i];
             }
 
-            for (int i = 1; i <= n; i++) {
-                for (int j = i; j <= n; j++) {
-                    cache[i][j] = Integer.MAX_VALUE;
-                    for (int k = i; k < j; k++) {
-                        cache[i][j] = Math.min(cache[i][j], cache[i][k] + cache[i][k + 1]);
-                    }
-                    for (int k = i; k <= j; k++) {
-                        cache[i][j] += a[k];
-                    }
+            for (int gap = 1; gap < n; gap++) {
+                for (int from = 1; from + gap <= n; from++) {
+                    int to = from + gap;
+                    cache[from][to] = Integer.MAX_VALUE; // 최소값 로직을 위해
+                    for (int i = from; i < to; i++)
+                        cache[from][to] = Math.min(cache[from][to],
+                                cache[from][i] + cache[i + 1][to] + sum[to] - sum[from - 1]);
                 }
             }
 
             sb.append(cache[1][n]).append("\n");
         }
-
         System.out.println(sb);
     }
 }
