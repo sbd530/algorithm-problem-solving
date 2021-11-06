0b8bca11bcdecc77f0aae24d9782c2ab4f5617b5
byungdon.yoon <byungdon92@gmail.com>
2021-11-06 16:03:03 +0900

    2021.11.06

---
+++ src/main/java/baekjoon/bruteForce1/boj14889/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.bruteForce1.boj14889;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
